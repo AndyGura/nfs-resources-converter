a67936cb8e084833ff6db6b3924e3d843f7d0d09
andy <andy.gura.jd@gmail.com>
2026-03-28 16:14:02 +0200

    try to fix macOS file handling

--- actions/gui_editor.py
+++ actions/gui_editor.py
@@ -30,6 +30,24 @@ def run_gui_editor(file_path=None):
     shutil.copytree(_get_frontend_dist_path(), static_path, dirs_exist_ok=True)
 
     api = API(static_path, file_path)
+
+    if sys.platform == 'darwin':
+        try:
+            from AppKit import NSApplication, NSObject
+
+            class _AppDelegate(NSObject):
+                def application_openFile_(self, app, filename):
+                    # check if eel is initialized and running
+                    if eel._websockets:
+                        eel.open_file(filename)
+                    return True
+
+            _app = NSApplication.sharedApplication()
+            _delegate = _AppDelegate.alloc().init()
+            _app.setDelegate_(_delegate)
+        except Exception:
+            pass
+
     eel.init(static_path)
     try:
         eel.start('index.html', port=0)
