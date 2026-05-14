ff390945fe7478a5f77c961a0c0914b90dd12b7c
Andy <andy.gura.jd@gmail.com>
2026-05-14 03:04:10 +0300

    fixed random crash by enqueuing eel calls

--- frontend/src/app/services/eel-delegate.service.local.ts
+++ frontend/src/app/services/eel-delegate.service.local.ts
@@ -3,10 +3,10 @@ import { MatDialog } from '@angular/material/dialog';
 import { BehaviorSubject } from 'rxjs';
 import { BlockData, CustomAction, ReadError, Resource, ResourceError } from '../components/editor/types';
 import { ErrorDialogComponent } from '../components/error.dialog/error.dialog.component';
+import { EelDelegateImplService } from './eel-delegate-impl/eel-delegate-impl.service';
 
-declare const eel: { expose: (func: Function, alias: string) => void } & { [key: string]: Function; _websocket: any };
-
-type GeneralConfig = {
+// These types are used by consumers of this service
+export type GeneralConfig = {
   blender_executable: string;
   ffmpeg_executable: string;
   print_errors: boolean;
@@ -15,7 +15,7 @@ type GeneralConfig = {
   show_hidden_fields: boolean;
 };
 
-type ConversionConfig = {
+export type ConversionConfig = {
   multiprocess_processes_count: number;
   input_path: string;
   output_path: string;
@@ -30,56 +30,42 @@ type ConversionConfig = {
   geometry__export_to_gg_web_engine: boolean;
 };
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable()
 export class EelDelegateService {
   public readonly openedResource$: BehaviorSubject<Resource | ResourceError | null> = new BehaviorSubject<
     Resource | ResourceError | null
   >(null);
   public readonly openedResourcePath$: BehaviorSubject<string | null> = new BehaviorSubject<string | null>(null);
   public readonly recentFiles$: BehaviorSubject<string[]> = new BehaviorSubject<string[]>([]);
-  public readonly conversionProgress$: BehaviorSubject<[number, number]> = new BehaviorSubject([0, 0]);
+  public readonly conversionProgress$: BehaviorSubject<[number, number]> = new BehaviorSubject<[number, number]>([0, 0]);
   public readonly version$: BehaviorSubject<string> = new BehaviorSubject<string>('');
 
   public changedDataBlocks: { [key: string]: any } = {};
 
+  readonly _impl: EelDelegateImplService;
+
   constructor(private readonly ngZone: NgZone, private readonly dialog: MatDialog) {
-    eel.expose(this.wrapHandler(this.openFile), 'open_file');
-    eel.expose(this.wrapHandler(this.updateConversionProgress), 'update_conversion_progress');
-    eel['on_angular_ready']();
-    this.syncRecentFiles().then();
-    this.syncVersion().then();
-    // wait while eel websocket connection establishes and add a handler to close window when main python script stopped
-    setTimeout(async () => {
-      while (true) {
-        if (eel._websocket) {
-          eel._websocket.onclose = () => window.close();
-          break;
-        }
-        await new Promise(r => setTimeout(r, 0));
-      }
-    }, 0);
+    this._impl = new EelDelegateImplService(this.ngZone);
+    this._impl.openedResource$.subscribe((v: any) => {
+      if (this.openedResource$.getValue() !== v) this.openedResource$.next(v);
+    });
+    this._impl.openedResourcePath$.subscribe((v: any) => {
+      if (this.openedResourcePath$.getValue() !== v) this.openedResourcePath$.next(v);
+    });
+    this._impl.recentFiles$.subscribe((v: any) => {
+      if (this.recentFiles$.getValue() !== v) this.recentFiles$.next(v);
+    });
+    this._impl.conversionProgress$.subscribe((v: any) => {
+      if (this.conversionProgress$.getValue() !== v) this.conversionProgress$.next(v);
+    });
+    this._impl.version$.subscribe((v: any) => {
+      if (this.version$.getValue() !== v) this.version$.next(v);
+    });
   }
 
-  private wrapHandler(handler: (...args: any[]) => unknown) {
-    return (...args: any[]) => {
-      try {
-        NgZone.assertInAngularZone();
-        handler.bind(this)(...args);
-      } catch (err: any) {
-        this.ngZone.run(() => {
-          try {
-            handler.bind(this)(...args);
-          } catch (innerErr: any) {
-            this.dialog.open(ErrorDialogComponent, {
-              data: { message: innerErr.message || innerErr.errorText || innerErr.toString() },
-            });
-            throw innerErr;
-          }
-        });
-      }
-    };
+
+  private async getImpl() {
+    return this._impl;
   }
 
   private async runSafe<T>(func: () => Promise<T>): Promise<T> {
@@ -94,79 +80,56 @@ export class EelDelegateService {
   }
 
   public async openFile(path: string, forceReload: boolean = false) {
-    return this.runSafe(async () => {
-      if (!path) {
-        return;
-      }
-      this.openedResource$.next(null);
-      this.openedResourcePath$.next(path);
-      const res: Omit<Resource, 'id'> | Omit<ResourceError, 'id'> = await eel['open_file'](path, forceReload)();
-      this.openedResource$.next({ ...res, id: res.name });
-      await this.syncRecentFiles();
-    });
+    return this.runSafe(async () => (await this.getImpl()).openFile(path, forceReload));
   }
 
   public async syncVersion() {
-    return this.runSafe(async () => {
-      const version = await eel['get_version']()();
-      this.version$.next(version);
-    });
+    return this.runSafe(async () => (await this.getImpl()).syncVersion());
   }
 
   public async syncRecentFiles() {
-    const cfg = await this.getGeneralConfig();
-    return this.recentFiles$.next(cfg.recent_files || []);
+    return this.runSafe(async () => (await this.getImpl()).syncRecentFiles());
   }
 
   updateConversionProgress(current: number, total: number): void {
-    try {
-      this.conversionProgress$.next([current, total]);
-    } catch (err: any) {
-      this.dialog.open(ErrorDialogComponent, {
-        data: { message: err.message || err.errorText || err.toString() },
-      });
-      throw err;
-    }
+    this.getImpl().then(impl => {
+      try {
+        impl.updateConversionProgress(current, total);
+      } catch (err: any) {
+        this.dialog.open(ErrorDialogComponent, {
+          data: { message: err.message || err.errorText || err.toString() },
+        });
+        throw err;
+      }
+    });
   }
 
   public async openFileDialog(multiple: boolean = false): Promise<string[]> {
-    return this.runSafe(async () => await eel['open_file_dialog'](multiple)());
+    return this.runSafe(async () => (await this.getImpl()).openFileDialog(multiple));
   }
 
   public async saveFileDialog(fileName?: string): Promise<string | null> {
-    return this.runSafe(async () => await eel['save_file_dialog'](fileName || null)());
+    return this.runSafe(async () => (await this.getImpl()).saveFileDialog(fileName));
   }
 
   public async openFileWithSystemApp(path: string) {
-    return this.runSafe(async () => {
-      await eel['open_file_with_system_app'](path)();
-    });
+    return this.runSafe(async () => (await this.getImpl()).openFileWithSystemApp(path));
   }
 
   public async retrieveValue<T = any>(id: string): Promise<T> {
-    return this.runSafe(async () => await eel['retrieve_value'](id)());
+    return this.runSafe(async () => (await this.getImpl()).retrieveValue(id));
   }
 
   public async runCustomAction(name: string, action: CustomAction, args: { [key: string]: any }) {
-    return this.runSafe(async () => eel['run_custom_action'](name, action, args)());
+    return this.runSafe(async () => (await this.getImpl()).runCustomAction(name, action, args));
   }
 
   public async getNewItemData(id: string): Promise<any> {
-    return this.runSafe(async () => eel['get_new_item_data'](id)());
+    return this.runSafe(async () => (await this.getImpl()).getNewItemData(id));
   }
 
   public async saveFile(changes: { id: string; value: any }[]): Promise<void> {
-    return this.runSafe(async () => {
-      const current = this.openedResource$.getValue();
-      if (!current) return;
-      const updatedData = await eel['save_file'](this.openedResourcePath$.getValue(), changes)();
-      this.openedResource$.next({
-        id: current.id,
-        name: current.name,
-        schema: current.schema,
-        data: updatedData,
-      });
-    });
+    return this.runSafe(async () => (await this.getImpl()).saveFile(changes));
   }
 
   public async serializeResource(blockId: string, path: string | null = null, settingsPatch: any = {}): Promise<string[]> {
@@ -175,35 +138,35 @@ export class EelDelegateService {
       .map(([id, value]) => {
         return { id, value };
       });
-    return this.runSafe(async () => eel['serialize_resource'](blockId, path, changes, settingsPatch)());
+    return this.runSafe(async () => (await this.getImpl()).serializeResource(blockId, path, changes as any, settingsPatch));
   }
 
   public async deserializeResource(id: string, filePaths: string[], extraOpts: any = {}): Promise<BlockData | ReadError> {
-    return this.runSafe(async () => eel['deserialize_resource'](id, filePaths, extraOpts)());
+    return this.runSafe(async () => (await this.getImpl()).deserializeResource(id, filePaths, extraOpts));
   }
 
   public async selectDirectoryDialog(): Promise<string | null> {
-    return this.runSafe(async () => await eel['select_directory_dialog']()());
+    return this.runSafe(async () => (await this.getImpl()).selectDirectoryDialog());
   }
 
   public async getGeneralConfig(): Promise<GeneralConfig> {
-    return this.runSafe(async () => await eel['get_general_config']()());
+    return this.runSafe(async () => (await this.getImpl()).getGeneralConfig());
   }
 
   public async getConversionConfig(): Promise<ConversionConfig> {
-    return this.runSafe(async () => await eel['get_conversion_config']()());
+    return this.runSafe(async () => (await this.getImpl()).getConversionConfig());
   }
 
   public async patchGeneralConfig(data: Partial<GeneralConfig>): Promise<GeneralConfig> {
-    return this.runSafe(async () => await eel['patch_general_config'](data)());
+    return this.runSafe(async () => (await this.getImpl()).patchGeneralConfig(data));
   }
 
   public async patchConversionConfig(data: Partial<ConversionConfig>): Promise<ConversionConfig> {
-    return this.runSafe(async () => await eel['patch_conversion_config'](data)());
+    return this.runSafe(async () => (await this.getImpl()).patchConversionConfig(data));
   }
 
   public async testExecutable(executablePath: string): Promise<any> {
-    return this.runSafe(async () => await eel['test_executable'](executablePath)());
+    return this.runSafe(async () => (await this.getImpl()).testExecutable(executablePath));
   }
 
   public async convertFiles(
@@ -211,21 +174,14 @@ export class EelDelegateService {
     outputPath: string,
     settings?: any,
   ): Promise<{ success: boolean; error?: string; output_path?: string }> {
-    return this.runSafe(async () => await eel['convert_files'](inputPath, outputPath, settings)());
+    return this.runSafe(async () => (await this.getImpl()).convertFiles(inputPath, outputPath, settings));
   }
 
   public async startFile(path: string): Promise<{ success: boolean; error?: string }> {
-    return this.runSafe(async () => await eel['start_file'](path)());
+    return this.runSafe(async () => (await this.getImpl()).startFile(path));
   }
 
   public async closeFile(): Promise<{ success: boolean; message: string }> {
-    return this.runSafe(async () => {
-      const result = await eel['close_file']()();
-      if (result.success) {
-        this.openedResource$.next(null);
-        this.openedResourcePath$.next(null);
-      }
-      return result;
-    });
+    return this.runSafe(async () => (await this.getImpl()).closeFile());
   }
 }
